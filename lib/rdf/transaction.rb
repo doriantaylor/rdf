2adef09015f4bff163691d4a8f1bec6c29b1728c
Dorian Taylor <code@doriantaylor.com>
2026-05-28 16:57:55 -0400

    fixed whitespace and options

--- lib/rdf/transaction.rb
+++ lib/rdf/transaction.rb
@@ -4,21 +4,24 @@ module RDF
   #
   # Transactions provide an ACID scope for queries and mutations.
   #
-  # Repository implementations may provide support for transactional updates
-  # by providing an atomic implementation of {Mutable#apply_changeset} and 
-  # responding to `#supports?(:atomic_write)` with `true`.
-  # 
-  # We carefully distinguish between read-only and read/write transactions,
-  # in order to enable repository implementations to take out the
-  # appropriate locks for concurrency control. Transactions are read-only
-  # by default; mutability must be explicitly requested on construction in
-  # order to obtain a read/write transaction.
+  # Repository implementations may provide support for transactional
+  # updates by providing an atomic implementation of
+  # {Mutable#apply_changeset} and responding to
+  # `#supports?(:atomic_write)` with `true`.
   #
-  # Individual repositories may make their own sets of guarantees within the 
-  # transaction's scope. In case repository implementations should be unable
-  # to provide full ACID guarantees for transactions, that must be clearly 
-  # indicated in their documentation. If update atomicity is not provided, 
-  # `#supports?(:atomic_write)` must respond `false`.
+  # We carefully distinguish between read-only and read/write
+  # transactions, in order to enable repository implementations to
+  # take out the appropriate locks for concurrency
+  # control. Transactions are read-only by default; mutability must be
+  # explicitly requested on construction in order to obtain a
+  # read/write transaction.
+  #
+  # Individual repositories may make their own sets of guarantees
+  # within the transaction's scope. In case repository implementations
+  # should be unable to provide full ACID guarantees for transactions,
+  # that must be clearly indicated in their documentation. If update
+  # atomicity is not provided, `#supports?(:atomic_write)` must
+  # respond `false`.
   #
   # @example Executing a read-only transaction
   #   repository = RDF::Repository.new
@@ -37,23 +40,25 @@ module RDF
   #     tx.delete [subject, RDF::RDFS.label, "Old title"]
   #     tx.insert [subject, RDF::RDFS.label, "New title"]
   #   end
-  # 
-  # The base class provides an atomic write implementation depending on
-  # {RDF::Changeset} and using {Changeset#apply}. Custom {Repository} classes
-  # can implement a minimial write-atomic transactions by overriding
-  # {#apply_changeset}.
   #
-  # Reads within a transaction run against the live repository by default
-  # ({#isolation_level} is `:read_committed`). Repositories may provide support
-  # for snapshots by implementing {Repository#snapshot} and responding `true` to
-  # `#supports?(:snapshots)`. In this case, the transaction will use the 
-  # {RDF::Dataset} returned by {#snapshot} for reads (`:repeatable_read`).
+  # The base class provides an atomic write implementation depending
+  # on {RDF::Changeset} and using {Changeset#apply}. Custom
+  # {Repository} classes can implement a minimial write-atomic
+  # transactions by overriding {#apply_changeset}.
   #
-  # For datastores that support transactions natively, implementation of a 
-  # custom {Transaction} subclass is recommended. The {Repository} is 
-  # responsible for specifying snapshot support and isolation level as 
-  # appropriate. Note that repositories may provide the snapshot isolation level
-  # without implementing {#snapshot}.
+  # Reads within a transaction run against the live repository by
+  # default ({#isolation_level} is `:read_committed`). Repositories
+  # may provide support for snapshots by implementing
+  # {Repository#snapshot} and responding `true` to
+  # `#supports?(:snapshots)`. In this case, the transaction will use
+  # the {RDF::Dataset} returned by {#snapshot} for reads
+  # (`:repeatable_read`).
+  #
+  # For datastores that support transactions natively, implementation
+  # of a custom {Transaction} subclass is recommended. The
+  # {Repository} is responsible for specifying snapshot support and
+  # isolation level as appropriate. Note that repositories may provide
+  # the snapshot isolation level without implementing {#snapshot}.
   #
   # @example A repository with a custom transaction class
   #  class MyRepository < RDF::Repository
@@ -129,26 +134,26 @@ module RDF
     # @yieldparam [RDF::Transaction] tx
     def initialize(repository, graph_name: nil, mutable: false, **options, &block)
       @repository = repository
-      @snapshot = 
+      @snapshot =
         repository.supports?(:snapshots) ? repository.snapshot : repository
       @options    = options.dup
       @mutable    = mutable
       @graph_name = graph_name
 
-      raise TransactionError, 
-            'Tried to open a mutable transaction on an immutable repository' if
+      raise TransactionError,
+        'Tried to open a mutable transaction on an immutable repository' if
         @mutable && !@repository.mutable?
 
       @changes = RDF::Changeset.new
-      
+
       if block_given?
         case block.arity
-          when 1 then block.call(self)
-          else self.instance_eval(&block)
+        when 1 then block.call(self)
+        else self.instance_eval(&block)
         end
       end
     end
-    
+
     ##
     # @see RDF::Dataset#isolation_level
     def isolation_level
@@ -157,26 +162,27 @@ module RDF
     end
 
     ##
-    # Indicates whether the transaction includes changes relative to the target 
-    # repository's state at transaction start time.
+    # Indicates whether the transaction includes changes relative to
+    # the target repository's state at transaction start time.
     #
-    # The response is guaranteed to be `true` if executing the transaction 
-    # against the original repository state would cause a change. It may also
-    # return `true` in cases where the repository would not change (e.g. 
-    # because the transaction would insert statements already present). 
+    # The response is guaranteed to be `true` if executing the
+    # transaction against the original repository state would cause a
+    # change. It may also return `true` in cases where the repository
+    # would not change (e.g.  because the transaction would insert
+    # statements already present).
     #
     # @note `Transaction` implementers may choose to `NotImplementedError`
     #   if the transaction implementation cannot be implemented efficiently.
     #
-    # @return [Boolean] true if the transaction has mutated (insert/delete) 
-    #   since transaction start time
+    # @return [Boolean] true if the transaction has mutated
+    #   (insert/delete) since transaction start time
     #
     # @raise [NotImplementedError] if a mutation check is not implemented
     def mutated?
       return !changes.empty? if self.class == Transaction
 
-      raise NotImplementedError, 
-            '#mutated? is not implemented for #{self.class}'
+      raise NotImplementedError,
+        '#mutated? is not implemented for #{self.class}'
     end
 
     ##
@@ -217,7 +223,8 @@ module RDF
       case args.length
       when 0 then false
       when 1 then read_target.statement?(*args)
-      else raise ArgumentError("wrong number of arguments (given #{args.length}, expected 0 or 1)")
+      else raise ArgumentError,
+          "wrong number of arguments (given #{args.length}, expected 0 or 1)"
       end
     end
     alias_method :has_statement?, :statement?
@@ -247,16 +254,17 @@ module RDF
     # @raise [TransactionError] if the transaction can't be applied
     def execute
       raise TransactionError, 'Cannot execute a rolled back transaction. ' \
-                              'Open a new one instead.' if instance_variable_defined?(:@rolledback) && @rolledback
+        'Open a new one instead.' if
+        instance_variable_defined?(:@rolledback) && @rolledback
       @changes.apply(@repository)
     end
 
     ##
     # Rolls back the transaction
     #
-    # @note: the base class simply replaces its current `Changeset` with a 
-    #   fresh one. Other implementations may need to explictly rollback 
-    #   at the supporting datastore.
+    # @note: the base class simply replaces its current `Changeset`
+    #   with a fresh one. Other implementations may need to explictly
+    #   rollback at the supporting datastore.
     #
     # @note: clients should not rely on using same transaction instance after
     #   rollback.
@@ -289,21 +297,20 @@ module RDF
       @changes.delete(process_statement(statement))
     end
 
-    def query_pattern(*args, &block)
-      read_target.send(:query_pattern, *args, &block)
+    def query_pattern(*args, **options, &block)
+      read_target.send(:query_pattern, *args, **options, &block)
     end
 
-    def query_execute(*args, &block)
-      read_target.send(:query_execute, *args, &block)
+    def query_execute(*args, **options, &block)
+      read_target.send(:query_execute, *args, **options, &block)
     end
-  
     undef_method :load, :update, :clear
 
     private
-    
+
     ##
-    # @private Adds the default graph_name to the statement, when one it does 
-    #   not already have one.
+    # @private Adds the default graph_name to the statement, when one
+    #   it does not already have one.
     #
     # @param statement [RDF::Statement]
     # @return [RDF::Statement]
@@ -314,7 +321,7 @@ module RDF
       end
       statement
     end
-    
+
     def read_target
       return @snapshot if graph_name.nil?
       return @snapshot.project_graph(nil) if graph_name == false
@@ -325,7 +332,7 @@ module RDF
 
     ##
     # A transaction with full serializability.
-    # 
+    #
     # @todo refactor me!
     # @see RDF::Transaction
     class SerializedTransaction < Transaction
@@ -335,15 +342,15 @@ module RDF
         super(*args, **options, &block)
         @base_snapshot = @snapshot
       end
-      
+
       ##
       # Inserts the statement to the transaction's working snapshot.
       #
       # @see Transaction#insert_statement
       def insert_statement(statement)
         @snapshot = @snapshot.class
-          .new(data: @snapshot.send(:insert_to, 
-                                    @snapshot.send(:data), 
+          .new(data: @snapshot.send(:insert_to,
+                                    @snapshot.send(:data),
                                     process_statement(statement)))
       end
 
@@ -353,8 +360,8 @@ module RDF
       # @see Transaction#insert_statement
       def delete_statement(statement)
         @snapshot = @snapshot.class
-          .new(data: @snapshot.send(:delete_from, 
-                                    @snapshot.send(:data), 
+          .new(data: @snapshot.send(:delete_from,
+                                    @snapshot.send(:data),
                                     process_statement(statement)))
       end
 
@@ -363,34 +370,36 @@ module RDF
       def isolation_level
         :serializable
       end
-      
+
       ##
       # @note this is a simple object equality check.
-      # 
+      #
       # @see RDF::Transaction#mutated?
       def mutated?
         !@snapshot.send(:data).equal?(repository.send(:data))
       end
-      
+
       ##
-      # Replaces repository data with the transaction's snapshot in a safely 
-      # serializable fashion.
-      # 
-      # @note this transaction uses a pessimistic merge strategy which 
-      #   fails the transaction if any data has changed in the repository
-      #   since transaction start time. However, the specific guarantee is 
-      #   softer: multiple concurrent conflicting transactions will not 
-      #   succeed. We may choose to implement a less pessimistic merge 
-      #   strategy as a non-breaking change.
-      # 
+      # Replaces repository data with the transaction's snapshot in a
+      # safely serializable fashion.
+      #
+      # @note this transaction uses a pessimistic merge strategy which
+      #   fails the transaction if any data has changed in the
+      #   repository since transaction start time. However, the
+      #   specific guarantee is softer: multiple concurrent
+      #   conflicting transactions will not succeed. We may choose to
+      #   implement a less pessimistic merge strategy as a
+      #   non-breaking change.
+      #
       # @raise [TransactionError] when the transaction can't be merged.
       # @see Transaction#execute
       def execute
         raise TransactionError, 'Cannot execute a rolled back transaction. ' \
-                                'Open a new one instead.' if instance_variable_defined?(:@rolledback) && @rolledback
+          'Open a new one instead.' if
+          instance_variable_defined?(:@rolledback) && @rolledback
 
         raise TransactionError, 'Error merging transaction. Repository' \
-                                'has changed during transaction time.' unless 
+          'has changed during transaction time.' unless
           repository.send(:data).equal? @base_snapshot.send(:data)
 
         repository.send(:data=, @snapshot.send(:data))
@@ -399,9 +408,9 @@ module RDF
 
     ##
     # An error class for transaction failures.
-    # 
-    # This error indicates that the transaction semantics have been violated in 
-    # some way.
+    #
+    # This error indicates that the transaction semantics have been
+    # violated in some way.
     class TransactionError < RuntimeError; end
   end # Transaction
 end # RDF
